561c49ea1833e5081ea35e3746c832c1e415e5fe
David Ojeda <david.ojeda.lopez@gmail.com>
2020-04-03 14:37:20 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Landing page
+
+This project shows a very basic landing page with placeholder titles and text.
+
+The navigation bar is built dynamically depending on the section Elements present in the HTML document. Each navigation link automatically scrolls to the given section on click.
+
+Also, sections are highlighted whenever the user is manually scrolling through the page. This is done using the [Intersection Observer API](https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API).
+
---
+++ js/app.js
@@ -0,0 +1,132 @@
+/**
+ *
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ *
+ * Dependencies: None
+ *
+ * JS Version: ES2015/ES6
+ *
+ * JS Standard: ESlint
+ *
+*/
+
+/**
+ * Define Global Variables
+ *
+*/
+
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ *
+*/
+
+const debounce = (func, delay) => {
+  let timerId;
+  return (...args) => {
+    const boundFunc = func.bind(this, ...args);
+    clearTimeout(timerId);
+    timerId = setTimeout(boundFunc, delay);
+  }
+};
+
+function createNavElement(staticElement) {
+  let navbarLink = document.createElement('a');
+  navbarLink.textContent = staticElement.dataset.nav;
+  navbarLink.dataset['sectionId'] = staticElement.id;
+  if(staticElement.id === 'section1') {
+    navbarLink.classList.toggle('active');
+  }
+  navbarLink.href = `#${staticElement.id}`;
+  return navbarLink;
+}
+
+function createNavbar() {
+  let navbar = document.querySelector('#navbar__list');
+  let sections = document.querySelectorAll('section[data-nav]');
+  // Using a for loop without any DocumentFragment makes more sense here since
+  // there are a low number of sections (< 10) and no degraded performance would be
+  // perceived.
+  for(let section of sections) {
+    let navbarElement = document.createElement('li');
+    navbarElement.appendChild(createNavElement(section));
+    navbar.appendChild(navbarElement);
+  }
+  return navbar;
+}
+
+function navbarClickHandler(event) {
+  // Not clicking a navbar link should just do nothing.
+  if(event.target.tagName === 'UL') {
+    return;
+  }
+
+  let navbar = document.querySelector('#navbar__list');
+  let currentActiveLink = navbar.querySelector('.active');
+  let clickedLink = event.target;
+  if(currentActiveLink == clickedLink) {
+    return;
+  }
+
+  currentActiveLink.classList.toggle('active');
+  clickedLink.classList.toggle('active');
+
+  let currentActiveSection = document.querySelector('section.active');
+  let newActiveSection = document.querySelector(`#${clickedLink.dataset.sectionId}`);
+  if(currentActiveSection == newActiveSection) {
+    return;
+  }
+
+  currentActiveSection.classList.toggle('active');
+  newActiveSection.classList.toggle('active');
+}
+
+function observerCallback(entries, observer) {
+  for(let entry of entries) {
+    if(entry.isIntersecting) {
+      let activeSection = document.querySelector('section.active');
+      if(activeSection) {
+        activeSection.classList.toggle('active');
+      }
+
+      document.querySelector('a.active').classList.toggle('active');
+
+      entry.target.classList.toggle('active');
+      document.querySelector(`a[data-section-id="${entry.target.id}"]`).classList.toggle('active');
+    }
+  }
+}
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+*/
+
+document.addEventListener('DOMContentLoaded', () => {
+  let navbar = createNavbar();
+  navbar.addEventListener('click', navbarClickHandler);
+
+  let observerOptions = {
+    root: null,
+    rootMargin: "0px",
+    threshold: .6
+  };
+  // Need to debounce this call to avoid sections to change its state to active
+  // when you the viewport is automatically scrolling. For example, being in section 1
+  // and clicking section 5 goes through sections 2, 3, and 4 and they should not be active
+  // at any moment through the scrolling event.
+  let observer = new IntersectionObserver(
+    debounce(observerCallback, 300), observerOptions
+  );
+
+  document.querySelectorAll('section').forEach(section => observer.observe(section));
+});
+
+/**
+ * End Main Functions
+*/
